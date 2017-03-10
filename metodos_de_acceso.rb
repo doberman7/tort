fdd76f4a5fb0f17617b5b6d6f35552c574194854
codeamac19 <codeamac19@gmail.com>
2017-03-10 17:43:09 -0600

    metodos de acceso ended

--- metodos_de_acceso.rb
+++ metodos_de_acceso.rb
@@ -1,31 +1,38 @@
-class Course 
+class Course
 
   def initialize(name)
     @course_name  = name
     @students   = []
   end
 
-  def course_name
-    @course_name
-  end
+  attr_accessor :course_name
+  attr_reader :students
 
-  def course_name=(new_name)
-    @course_name = new_name
-  end
+  # def course_name#ACCESOR
+  #   @course_name
+  # end
+  #
+  # def course_name=(new_name)#ACCESOR
+  #   @course_name = new_name
+  # end
 
-  def add_student(student)
+  def add_student(student)#WRITER
     @students << student
   end
 
-  def students
-    @students
-  end
+  # def students#READER
+  #   @students
+  # end
 end
 
 
 class Student
+  attr_accessor :name, :last_name, :campus
+  attr_reader :new_id
+  attr_reader :id
 
   @@id = 0
+
 
   def initialize(name, last_name, campus)
     @name     = name
@@ -34,37 +41,37 @@ class Student
     @id       = new_id
   end
 
-  def new_id
-    @@id += 1
-  end
+  # def new_id#reader
+  #   @@id += 1
+  # end
 
-  def name
-    @name
-  end
+  # def name#ACCESOR
+  #   @name
+  # end
+  #
+  # def name=(new_name)#ACESOR
+  #   @name = new_name
+  # end
 
-  def name=(new_name)
-    @name = new_name
-  end
+  # def last_name#ACCESOR
+  #   @last_name
+  # end
+  #
+  # def last_name=(new_last_name)#ACCESOR
+  #   @last_name = new_last_name
+  # end
 
-  def last_name
-    @last_name
-  end
+  # def campus#ACCESOR
+  #   @campus
+  # end
+  #
+  # def campus=(new_campus)#ACCESOR
+  #   @campus = new_campus
+  # end
 
-  def last_name=(new_last_name)
-    @last_name = new_last_name
-  end
-
-  def campus
-    @campus
-  end
-
-  def campus=(new_campus)
-    @campus = new_campus
-  end
-
-  def id
-    @id
-  end
+  # def id#reader
+  #   @id
+  # end
 
 end
 
