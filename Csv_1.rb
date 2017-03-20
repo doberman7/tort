9ca15dc92b513dda3c51c549bafd1fbe3aad3541
doberman7 <ivanrubioangeles@gmail.com>
2017-03-19 20:49:07 -0600

    csv_1.rb resuelto

--- Csv_1.rb
+++ Csv_1.rb
@@ -1,7 +1,7 @@
 require 'faker'#requerir la libreria faker, usada en METODO creater_people
 require 'csv'#requerir la libreria csv, usada en METODO create_csv
 class Person
-  attr_reader :first_name, :last_name, :email, :phone,:created_at
+  attr_accessor :first_name, :last_name, :email, :phone,:created_at
   def initialize(first_name, last_name, email, phone, created_at)
     @first_name = first_name
     @last_name = last_name
@@ -54,28 +54,58 @@ class PersonParser
   def modif(file, array)
     #leer el csv
     CSV.foreach(file) do |person_in_line|
-      #p "#{person_in_line[0]}, #{person_in_line[1]}, #{person_in_line[2]}, #{person_in_line[3]}, #{person_in_line[4]}"
+    #  p "#{person_in_line[0]}, #{person_in_line[1]}, #{person_in_line[2]}, #{person_in_line[3]}, #{person_in_line[4]}"
     end
-    #encontrar personas en el arreglo
-    #serched = gets.chomp
-    #p serched
-    #array
+    #p "que persona buscas quieres cambiar"
+    counter = 1
+
     array.each do |person_atributS|
-      #HACER UN CASE AQUI
-       p person_atributS.first_name
+      p "#{counter} #{person_atributS.first_name} #{person_atributS.last_name} #{person_atributS.phone} #{person_atributS.created_at}"
+      counter +=1
     end
+    p " selecciona su numero de persona"
+    number_to_look = gets.to_i
 
-    #escoger atributo
-    #modificar etributo
-    #regrasaer arreglo
-    #regrsar en CSV
+    p "que atributos quieres cambiar"
+    p "nombre presiona 1"
+    p "apellido presiona 2"
+    p "telefono presiona 3"
+    p "fecha de creacion presiona 4"
+    counter = 0
+    atribut_to_look = gets.to_i
+    #encontrar personas en el arreglo
+    array.each do |person_atributS|
+      counter +=1
+      #escoger atributo
+      if counter == number_to_look
+        p "ingresa el nuevo atributo"
+        new_atribut = gets.chomp
+        old_atribut = ""
+        #modificar atributo
+        case atribut_to_look
+        when 1 then old_atribut = person_atributS.first_name
+          when 2 then old_atribut = person_atributS.last_name
+          when 3 then old_atribut = person_atributS.phone
+          when 4 then old_atribut = person_atributS.created_at
+        end
+        case atribut_to_look
+          when 1 then person_atributS.first_name = new_atribut
+          when 2 then person_atributS.last_name = new_atribut
+          when 3 then person_atributS.phone = new_atribut
+          when 4 then person_atributS.created_at = new_atribut
+        end
+        p "#{old_atribut} ha sido modificado por #{new_atribut}"
+      end
 
-  	# CSV.open(file, "w+") do |line_in_csv|#open(fd, mode="r" [, opt]) { |io| block } → obj   El "w+" es "Read-write",
-  	#      array.each do |person|#itera sobre el arreglo contruido en el METODO creater_people
-  	#        line_in_csv << [person.first_name, person.last_name, person.email, person.phone, person.created_at]#person es cada uno de los objetosPersona del arreglo, el ".first_name" es la atributos del objeto,
-  	#      end
-  	#   end
-  end
+    end# fin de la iteracion del array.each do |person_atributS|
+
+    #empujar arreglo a CSV
+    CSV.open("people.csv", "wb") do |line_on_csv|
+      array.each do |person|
+        line_on_csv << [person.first_name, person.last_name, person.email, person.phone, person.created_at]
+      end
+    end
+  end#fin del metodo modif
 end
 
 people = creater_people(10)#asignar resultado del metodo creater_people
@@ -84,4 +114,4 @@ person_writer.create_csv
 
 parser = PersonParser.new('people.csv')
 people = parser.people
- parser.modif("people.csv", people)
+parser.modif("people.csv", people)
